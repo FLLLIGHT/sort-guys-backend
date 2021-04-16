eb4f1588e1b182294ea03163a2f546b2f620dca6
xuanzitao <xuan_zitao@126.com>
2021-04-16 11:01:45 +0800

    [Init] initial the project

---
+++ src/main/java/fudan/adweb/project/sortguysbackend/SortGuysBackendApplication.java
@@ -0,0 +1,13 @@
+package fudan.adweb.project.sortguysbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortGuysBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortGuysBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fudan/adweb/project/sortguysbackend/SortGuysBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fudan.adweb.project.sortguysbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortGuysBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
