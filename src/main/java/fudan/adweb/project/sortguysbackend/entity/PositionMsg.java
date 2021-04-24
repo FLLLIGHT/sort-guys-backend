a838e933f7d533e99f49487dd92009c5241df9c2
xuanzitao <xuan_zitao@126.com>
2021-04-25 00:28:30 +0800

    [ADD] try to implement position broadcast through ws

---
+++ src/main/java/fudan/adweb/project/sortguysbackend/entity/PositionMsg.java
@@ -0,0 +1,40 @@
+package fudan.adweb.project.sortguysbackend.entity;
+
+public class PositionMsg {
+    private String username;
+    private Double x;
+    private Double y;
+    private Double z;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Double getX() {
+        return x;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Double getY() {
+        return y;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
+
+    public Double getZ() {
+        return z;
+    }
+
+    public void setZ(Double z) {
+        this.z = z;
+    }
+}
---
+++ src/main/java/fudan/adweb/project/sortguysbackend/websocket/WebSocketPosition.java
@@ -0,0 +1,91 @@
+package fudan.adweb.project.sortguysbackend.websocket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import fudan.adweb.project.sortguysbackend.entity.PositionMsg;
+import fudan.adweb.project.sortguysbackend.entity.SocketMsg;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@ServerEndpoint(value = "/websocketPosition/{nickname}")
+@Component
+public class WebSocketPosition {
+    // bind sessionId and session
+    private static Map<String, Session> map = new HashMap<>();
+    // store WebSocketService
+    private static CopyOnWriteArraySet<WebSocketPosition> webSocketSet = new CopyOnWriteArraySet<>();
+    private static Map<String, String> usersMap = new HashMap<>();
+    private Session session;
+
+    /**
+     * on connect
+     */
+    @OnOpen
+    public void onOpen(Session session,@PathParam("nickname") String nickname) {
+        Map<String,Object> message= new HashMap<>();
+        this.session = session;
+        map.put(session.getId(), session);
+        usersMap.put(nickname, session.getId());
+        webSocketSet.add(this);
+
+        System.out.println("New connection: " + nickname + ", id: " + session.getId() + ", current people:" + webSocketSet.size());
+        message.put("type",0); // 0-connect success，1-message
+        message.put("people",webSocketSet.size());
+        message.put("name",nickname);
+        message.put("aisle",session.getId());
+        this.session.getAsyncRemote().sendText(new Gson().toJson(message));
+    }
+
+    /**
+     * close
+     */
+    @OnClose
+    public void onClose() {
+        webSocketSet.remove(this);
+        map.remove(this.session.getId());
+        for (Map.Entry<String, String> entry : usersMap.entrySet()){
+            if (entry.getValue().equals(this.session.getId())){
+                usersMap.remove(entry.getKey());
+                break;
+            }
+        }
+        System.out.println("Close 1, current people: " + webSocketSet.size());
+    }
+
+    /**
+     * @param message message from client
+     */
+    @OnMessage
+    public void onMessage(String message, Session session,@PathParam("nickname") String nickname) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        PositionMsg positionMsg;
+
+        try {
+            positionMsg = objectMapper.readValue(message, PositionMsg.class);
+            broadcast(positionMsg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+    // 广播位置信息
+    private void broadcast(PositionMsg positionMsg) {
+        for (WebSocketPosition item : webSocketSet) {
+            item.session.getAsyncRemote().sendObject(positionMsg);
+        }
+    }
+
+}
