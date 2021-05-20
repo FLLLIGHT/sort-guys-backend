5b074487511db96c8f25c6125eb4205b5c6cae18
xuanzitao <xuan_zitao@126.com>
2021-05-20 19:36:49 +0800

    [Update] from broadcast to multicast (based on room id)

--- src/main/java/fudan/adweb/project/sortguysbackend/websocket/WebSocketPosition.java
+++ src/main/java/fudan/adweb/project/sortguysbackend/websocket/WebSocketPosition.java
@@ -11,35 +11,60 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-@ServerEndpoint(value = "/websocketPosition/{nickname}")
+@ServerEndpoint(value = "/websocketPosition/{roomId}/{nickname}")
 @Component
 public class WebSocketPosition {
-    // bind sessionId and session
-    private static Map<String, Session> map = new HashMap<>();
-    // store WebSocketService
-    private static CopyOnWriteArraySet<WebSocketPosition> webSocketSet = new CopyOnWriteArraySet<>();
+//    private static CopyOnWriteArraySet<WebSocketPosition> webSocketSet = new CopyOnWriteArraySet<>();
     private static Map<String, String> usersMap = new HashMap<>();
-    private static Map<String, PositionMsg> positionMap = new HashMap<>();
+//    private static Map<String, PositionMsg> positionMap = new HashMap<>();
+
+    // 记录房间内的用户（及session）
+    private static final Map<Integer, Set<WebSocketPosition>> roomMap = new ConcurrentHashMap<>();
+
+    // 记录房间内用户的位置
+    private static final Map<Integer, Map<String, PositionMsg>> roomPositionMap = new ConcurrentHashMap<>();
+
     private Session session;
 
     /**
      * on connect
      */
     @OnOpen
-    public void onOpen(Session session,@PathParam("nickname") String nickname) throws IOException {
+    public void onOpen(Session session, @PathParam("roomId") Integer roomId, @PathParam("nickname") String nickname) throws IOException {
+
         Map<String,Object> message= new HashMap<>();
         this.session = session;
-        map.put(session.getId(), session);
         usersMap.put(nickname, session.getId());
         PositionMsg positionMsg = new PositionMsg(nickname, 0d, 30d, 0d, 1);
-        positionMap.put(nickname, positionMsg);
-        webSocketSet.add(this);
 
-        System.out.println("New connection: " + nickname + ", id: " + session.getId() + ", current people:" + webSocketSet.size());
+        Map<String, PositionMsg> positionMap = roomPositionMap.get(roomId);
+        // 若房间不存在，则创建新房间；若房间存在，则将用户放入房间
+        if (positionMap == null){
+            positionMap = new HashMap<>();
+        }
+        positionMap.put(nickname, positionMsg);
+        roomPositionMap.put(roomId, positionMap);
+
+//        webSocketSet.add(this);
+
+        Set<WebSocketPosition> room = roomMap.get(roomId);
+        // 若房间不存在，则创建新房间；若房间存在，则将用户放入房间
+        if (room == null){
+            room = new CopyOnWriteArraySet<>();
+            room.add(this);
+            roomMap.put(roomId, room);
+        }else{
+            room.add(this);
+        }
+
+
+        System.out.println("New connection: " + nickname + ", id: " + session.getId() + ", current people:" + roomMap.get(roomId).size());
         message.put("type",0); // 0-connect success，1-message
-        message.put("people",webSocketSet.size());
+        message.put("people",roomMap.get(roomId).size());
         message.put("name",nickname);
         message.put("aisle",session.getId());
         synchronized (this.session){
@@ -48,16 +73,19 @@ public class WebSocketPosition {
         synchronized (this.session){
             this.session.getBasicRemote().sendText(asJsonString(positionMap));
         }
-        broadcast(positionMsg);
+        multicast(positionMsg, roomId);
     }
 
     /**
      * close
      */
     @OnClose
-    public void onClose() throws IOException {
-        webSocketSet.remove(this);
-        map.remove(this.session.getId());
+    public void onClose(Session session, @PathParam("roomId") Integer roomId, @PathParam("nickname") String nickname) throws IOException {
+//        webSocketSet.remove(this);
+        if(roomMap.containsKey(roomId)){
+            roomMap.get(roomId).remove(this);
+        }
+
         String username = "";
         for (Map.Entry<String, String> entry : usersMap.entrySet()){
             if (entry.getValue().equals(this.session.getId())){
@@ -66,21 +94,20 @@ public class WebSocketPosition {
                 break;
             }
         }
-        for (Map.Entry<String, PositionMsg> entry : positionMap.entrySet()){
+        for (Map.Entry<String, PositionMsg> entry : roomPositionMap.get(roomId).entrySet()){
             if (entry.getKey().equals(username)){
-                positionMap.remove(entry.getKey());
+                roomPositionMap.get(roomId).remove(entry.getKey());
                 break;
             }
         }
-        System.out.println("Close 1, current people: " + webSocketSet.size());
-        broadcast(new PositionMsg(username, -1d, -1d, -1d, 2));
+        multicast(new PositionMsg(username, -1d, -1d, -1d, 2), roomId);
     }
 
     /**
      * @param message message from client
      */
     @OnMessage
-    public void onMessage(String message, Session session,@PathParam("nickname") String nickname) {
+    public void onMessage(String message, Session session, @PathParam("roomId") Integer roomId, @PathParam("nickname") String nickname) {
         ObjectMapper objectMapper = new ObjectMapper();
         PositionMsg positionMsg;
 
@@ -88,8 +115,8 @@ public class WebSocketPosition {
             positionMsg = objectMapper.readValue(message, PositionMsg.class);
             // 设置正常消息
             positionMsg.setType(1);
-            positionMap.put(nickname, positionMsg);
-            broadcast(positionMsg);
+            roomPositionMap.get(roomId).put(nickname, positionMsg);
+            multicast(positionMsg, roomId);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -101,9 +128,10 @@ public class WebSocketPosition {
         error.printStackTrace();
     }
 
-    // 广播位置信息
-    private void broadcast(PositionMsg positionMsg) throws IOException {
-        for (WebSocketPosition item : webSocketSet) {
+    // 组播位置信息（基于房间号）
+    private void multicast(PositionMsg positionMsg, Integer roomId) throws IOException {
+        Set<WebSocketPosition> room = roomMap.get(roomId);
+        for (WebSocketPosition item : room) {
             synchronized (item.session){
                 item.session.getBasicRemote().sendText(asJsonString(positionMsg));
             }
