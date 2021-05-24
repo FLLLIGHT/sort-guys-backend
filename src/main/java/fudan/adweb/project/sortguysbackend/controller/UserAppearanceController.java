45fc792ff0f7a66bdb738065257cf2d31c30d76e
zhshy <18302010080@fudan.edu.cn>
2021-05-24 21:03:06 +0800

    [MODIFY]user cannot modify appearance of other users

--- src/main/java/fudan/adweb/project/sortguysbackend/controller/UserAppearanceController.java
+++ src/main/java/fudan/adweb/project/sortguysbackend/controller/UserAppearanceController.java
@@ -1,10 +1,12 @@
 package fudan.adweb.project.sortguysbackend.controller;
 
+import fudan.adweb.project.sortguysbackend.entity.User;
 import fudan.adweb.project.sortguysbackend.entity.UserAppearance;
 import fudan.adweb.project.sortguysbackend.service.UserAppearanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -36,6 +38,13 @@ public class UserAppearanceController {
 
     @PutMapping("/userAppearance/{uid}")
     public ResponseEntity<?> modifyAppearance(@PathVariable("uid") Integer uid, @RequestBody UserAppearance userAppearance){
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (!user.getUid().equals(uid)){
+            Map<String, String> map = new HashMap<>();
+            map.put("message", "只能修改自己的虚拟形象");
+            return new ResponseEntity<>(map, HttpStatus.FORBIDDEN);
+        }
+
         ResponseEntity.BodyBuilder builder = ResponseEntity.ok();
         String message = userAppearanceService.updateAppearance(uid, userAppearance);
         Map<String, String> map = new HashMap<>();
