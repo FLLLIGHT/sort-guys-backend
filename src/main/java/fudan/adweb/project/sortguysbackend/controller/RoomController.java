b6a74bbe7d81cf840312cac5196a367415da38a0
zhshy <18302010080@fudan.edu.cn>
2021-05-26 14:06:40 +0800

    [ADD]add user info check for creating room

--- src/main/java/fudan/adweb/project/sortguysbackend/controller/RoomController.java
+++ src/main/java/fudan/adweb/project/sortguysbackend/controller/RoomController.java
@@ -3,10 +3,13 @@ package fudan.adweb.project.sortguysbackend.controller;
 import fudan.adweb.project.sortguysbackend.controller.request.AuthRequest;
 import fudan.adweb.project.sortguysbackend.controller.request.RoomRequest;
 import fudan.adweb.project.sortguysbackend.entity.GarbageSortResultInfo;
+import fudan.adweb.project.sortguysbackend.entity.User;
 import fudan.adweb.project.sortguysbackend.entity.game.RoomInfo;
 import fudan.adweb.project.sortguysbackend.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -25,6 +28,12 @@ public class RoomController {
 
     @PostMapping("/room")
     public ResponseEntity<?> createNewRoom(@RequestBody RoomRequest request) {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (!user.getUsername().equals(request.getUsername())){
+            Map<String, String> map = new HashMap<>();
+            map.put("message", "不能为其他用户创建房间");
+            return new ResponseEntity<>(map, HttpStatus.FORBIDDEN);
+        }
         long newRoomId = roomService.getAvailableRoomIdAndIncr();
         String roomId = roomService.createRoom(String.valueOf(newRoomId), request.getUsername());
         ResponseEntity.BodyBuilder builder = ResponseEntity.ok();
